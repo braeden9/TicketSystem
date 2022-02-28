aaaf2d842b82e38736dfe9703bb2838a854609fa
Braeden <bcook9@my.wctc.edu>
2022-02-28 10:36:41 -0600

    [1.00] init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TicketSysClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
